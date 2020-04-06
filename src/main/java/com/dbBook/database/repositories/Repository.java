ca9177abad6ca0d2c223e70a43f4961dcc4478df
RomaPress <romapres00@gmail.com>
2020-04-06 18:15:31 +0300

    create all assembly function

--- src/main/java/com/dbBook/database/repositories/Repository.java
+++ src/main/java/com/dbBook/database/repositories/Repository.java
@@ -6,6 +6,7 @@ import com.dbBook.database.MyConnectionFactory;
 
 public interface Repository {
     default Connection getConnection() {
+
         return MyConnectionFactory.getConnection();
     }
 }
